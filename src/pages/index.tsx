f23f199d4c1e0bb3b159c54e8e4b83ee2f347a9e
MatDev435 <matheuspsalmazo2@gmail.com>
2024-03-08 11:05:52 -0300

    Estrutura visual: Configurando o Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  '&:hover': {
+    backgroundColor: '#fff'
+  }
+})
+
 export default function Home() {
   return (
-    <div>Hello World!</div>
+    <Button>Enviar</Button>
   )
 }
